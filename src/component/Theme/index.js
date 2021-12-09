d948392079c2b76439449ab56469d3dfe2a3ea81
neginkhatami <93207263+neginkhatami@users.noreply.github.com>
2021-12-09 10:41:30 +0330

    Update theme

--- src/component/Theme/index.js
+++ src/component/Theme/index.js
@@ -3,34 +3,42 @@ import {createMuiTheme} from "@material-ui/core";
 const Theme = createMuiTheme({
     palette:{
       primary:{
-          main:"#191970",
+          main:"#1E90FF",
       },
       secondary:{
           main:"#FFFAF0",
       }
     },
-    overrides:{
+    overrides: {
         MuiAppBar: {
             positionFixed: {
                 position: "sticky !important"
             }
-        },
-        MuiInputBase:{
-            input:{
-                width : "270px "
+        }, MuiInputBase: {
+            input: {
+                fontFamily : "shabnam !important",
+                padding : "6px  !important",
+                width : "330px",
+                fontSize: "14px"
             }
-        },
-        MuiTypography :{
-            root :{
+        }, MuiTypography: {
+            root: {
                 fontFamily: "Shabnam !important",
+            },
+            body1:{
+                marginRight : "0.5rem !important"
             }
-        },MuiButton:{
-            label:{
+        }, MuiButton: {
+            label: {
                 fontFamily: "shabnam !important",
             }
-        },MuiPaper:{
+        },MuiFormControlLabel: {
             root:{
-                backgroundColor: "secondary",
+                marginLeft: "15rem !important",
+                marginTop: "1.5rem !important"
+            },
+            label:{
+                fontSize: "0.8rem !important"
             }
         }
     }
